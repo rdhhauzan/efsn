714fddbd3111f1fbf81f976d78c38c2768a546c7
Philip Fan <fanwengang@hotmail.com>
2021-10-22 10:18:59 +0800

    test: miner logic for pending block

--- miner/worker.go
+++ miner/worker.go
@@ -860,14 +860,17 @@ func (w *worker) commitNewWork(interrupt *int32, noempty bool, timestamp int64) 
 		}
 		header.Coinbase = w.coinbase
 	}
-	if err := w.engine.Prepare(w.chain, header); err != nil {
-		switch err {
-		case datong.ErrNoTicket:
-			common.DebugInfo("Miner doesn't have ticket", "number", parent.Number())
-		default:
-			log.Error("Failed to prepare header for mining", "err", err)
+	// Fusion use TPOS instead of POW, so only mining mode is required to calculate the difficulty, nonce,
+	if w.isRunning() {
+		if err := w.engine.Prepare(w.chain, header); err != nil {
+			switch err {
+			case datong.ErrNoTicket:
+				common.DebugInfo("Miner doesn't have ticket", "number", parent.Number())
+			default:
+				log.Error("Failed to prepare header for mining", "err", err)
+			}
+			return
 		}
-		return
 	}
 	// If we are care about TheDAO hard-fork check whether to override the extra-data or not
 	if daoBlock := w.chainConfig.DAOForkBlock; daoBlock != nil {
